53be6d56ba00d89fdaaebb3637db55f9d514f06f
erdemakatay <erdemakatay@gmail.com>
2026-05-08 16:35:52 +0300

    Initial commit

---
+++ app/src/main/java/com/turkcell/ticketapp/TicketAppAplication.kt
@@ -0,0 +1,22 @@
+package com.turkcell.ticketapp
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+// Uygulama başladığında activitylerden önce oluşturulur.
+// Singleton (uygulama yaşadığı sürece , tek bir instance olarak memoryde kalır)
+// Uygulama kapanana kadar yok edilmez...
+class TicketAppAplication : Application() {
+    override fun onCreate () {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@TicketAppAplication)
+            modules(
+
+            )
+        }
+    }
+}
